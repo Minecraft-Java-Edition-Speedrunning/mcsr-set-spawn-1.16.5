4dbe2685f7473c1f7873c0ea31ee1c7a70c57ba8
luna <voidxwalker77@gmail.com>
2022-09-12 01:30:43 +0200

    only x and z coordinates in config, cleaned up code, removed loading screen and debug modification

--- src/main/java/net/set/spawn/mod/SetSpawn.java
+++ src/main/java/net/set/spawn/mod/SetSpawn.java
@@ -3,16 +3,102 @@ package net.set.spawn.mod;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class SetSpawn implements ClientModInitializer {
-
+    public static Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "setspawnmod";
+    public static boolean shouldModifySpawn;
+    public static final String subDir = SetSpawn.MOD_ID + "1_16_5";
+    public static File configFile;
+    public static File globalConfigFile;
+    public static boolean use_global_config;
+    public static String seed;
+    public static double coordinateX;
+    public static double coordinateZ;
 
+    private static void createIfNonExistent(File file) {
+        try {
+            if(file.createNewFile()){
+                saveDefaultProperties(file);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void loadProperties() throws IOException,NumberFormatException {
+        Properties properties = getProperties(configFile);
+
+        use_global_config = Boolean.parseBoolean(properties.getProperty("use_global_config", "false"));
+        if (use_global_config) {
+            properties= getProperties(globalConfigFile);
+        }
+        seed = properties.getProperty("seed", "8398967436125155523");
+
+        try {
+            coordinateX= Double.parseDouble( properties.getProperty("coordinateX", "-201.5"));
+            coordinateZ= Double.parseDouble( properties.getProperty("coordinateZ", "229.5"));
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Coordinates given were in an invalid format. Not modifying player spawnpoint.");
+        }
+
+
+    }
+    static Properties getProperties(File file) throws IOException {
+        try(FileInputStream inputStream = new FileInputStream(file)){
+            Properties properties= new Properties();
+            properties.load(inputStream);
+            return properties;
+        }
+    }
+
+    private static void saveProperties(File file) throws IOException {
+        try(FileOutputStream f = new FileOutputStream(file)){
+            Properties properties = new Properties();
+            if (file == configFile) {
+                properties.put("use_global_config", ""+use_global_config);
+            }
+            properties.put("seed", seed);
+            properties.put("coordinateX", ""+coordinateX);
+            properties.put("coordinateZ", ""+coordinateZ);
+            properties.store(f, "");
+        }
+    }
+
+    private static void saveDefaultProperties(File file) throws IOException {
+        try(FileOutputStream f = new FileOutputStream(file)){
+            Properties properties = new Properties();
+            if (file == configFile) {
+                properties.put("use_global_config", "false");
+            }
+            properties.put("seed", "8398967436125155523");
+            properties.put("coordinateX", "-201.5");
+            properties.put("coordinateZ", "229.5");
+            properties.store(f, "");
+        }
+    }
     @Override
     public void onInitializeClient() {
-        if (FabricLoader.getInstance().isModLoaded("worldpreview")) {
-            LogManager.getLogger("SetSpawn").error("WorldPreview is not compatible with SetSpawn.");
-            System.exit(1);
+        LOGGER.info("Initializing");
+        File globalDir = new File (System.getProperty("user.home").replace("\\", "/"), subDir);
+        globalDir.mkdirs();
+        globalConfigFile = new File(globalDir, "globalsetspawn.properties");
+        configFile = FabricLoader.getInstance().getConfigDir().resolve("setspawn.properties").toFile();
+        createIfNonExistent(globalConfigFile);
+        createIfNonExistent(configFile);
+        try {
+            loadProperties();
+            saveProperties(configFile);
+            saveProperties(globalConfigFile);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/net/set/spawn/mod/mixin/MinecraftServerMixin.java
+++ src/main/java/net/set/spawn/mod/mixin/MinecraftServerMixin.java
@@ -1,151 +1,19 @@
 package net.set.spawn.mod.mixin;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.ServerTask;
 import net.minecraft.server.WorldGenerationProgressListener;
-import net.minecraft.server.command.CommandOutput;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.tag.BlockTags;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.snooper.SnooperListener;
-import net.minecraft.util.thread.ReentrantThreadExecutor;
-import net.minecraft.world.Heightmap;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.chunk.WorldChunk;
-import net.set.spawn.mod.Conditionals;
-import net.set.spawn.mod.config.SetSpawnProperties;
-import org.apache.logging.log4j.LogManager;
-import org.jetbrains.annotations.Nullable;
+import net.set.spawn.mod.SetSpawn;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public abstract class MinecraftServerMixin extends ReentrantThreadExecutor<ServerTask> implements SnooperListener, CommandOutput, AutoCloseable {
-
-    @Shadow public abstract ServerWorld getOverworld();
-
-    private static final double[] coordinates = new double[3];
-
-    public MinecraftServerMixin(String string) {
-        super(string);
-    }
+public abstract class MinecraftServerMixin{
 
     @Inject(method = "prepareStartRegion", at = @At(value = "HEAD"))
-    public void validateSpawn(WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci){
-        Conditionals.isModActive = false;
-        if (properSpawnEditingCircumstances(this.getOverworld())) {
-            Conditionals.isModActive = true;
-            double x = coordinates[0];
-            double y = coordinates[1];
-            double z = coordinates[2];
-            LogManager.getLogger("SetSpawn").info("Overriding player spawnpoint to " + x + " " + y + " " + z);
-        }
-    }
-
-    private boolean properSpawnEditingCircumstances(ServerWorld world){
-        if (!isEnabled()) return false;
-        if (!isWorldLoading()) return false;
-        if (!isSetSeed(world)) return false;
-        if (!loadCoordinates()) return false;
-        if (!areCoordinatesPossible(world)) return false;
-        return true;
-    }
-
-    private boolean isEnabled() {
-        if (!SetSpawnProperties.enabled) {
-            LogManager.getLogger("SetSpawn").info("Set Spawn is currently disabled.");
-        }
-        return SetSpawnProperties.enabled;
-    }
-
-    private boolean isWorldLoading(){
-        return Conditionals.isAWorldGenerating;
-    }
-
-    private boolean isSetSeed(ServerWorld world) {
-        return String.valueOf(world.getSeed()).equals(SetSpawnProperties.seed);
-    }
-
-    private boolean loadCoordinates() {
-        String[] coordinatesStringArray = SetSpawnProperties.coordinates.split(" ");
-        if (coordinatesStringArray.length != 3) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were in an invalid format. Not overriding player spawnpoint.");
-            return false;
-        }
-        try {
-            coordinates[0] = Double.parseDouble(coordinatesStringArray[0]);
-            coordinates[1] = Double.parseDouble(coordinatesStringArray[1]);
-            coordinates[2] = Double.parseDouble(coordinatesStringArray[2]);
-        } catch (NumberFormatException e) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were in an invalid format. Not overriding player spawnpoint.");
-            return false;
-        }
-        return true;
-    }
-
-    private boolean areCoordinatesPossible(ServerWorld world) {
-        double x = coordinates[0];
-        double y = coordinates[1];
-        double z = coordinates[2];
-        if ( (Math.abs(x) % 1 != 0.5) || (Math.abs(y) % 1 != 0.0) || (Math.abs(z) % 1 != 0.5) ) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were impossible. Make sure the coordinates are in <X>.5 <Y>.0 <Z>.5 format. Not overriding player spawnpoint.");
-            return false;
-        }
-
-        int spawnX = world.getSpawnPos().getX();
-        int spawnZ = world.getSpawnPos().getZ();
-        if ( (Math.abs(x - spawnX) > 10) || (Math.abs(z - spawnZ) > 10) ) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were impossible. Make sure X and Z are not more than 10 blocks away from the world spawn. Not overriding player spawnpoint.");
-            return false;
-        }
-
-        BlockPos setCoords = new BlockPos(x, y, z);
-        double overworldSpawnY = findOverworldSpawn(world, setCoords.getX(), setCoords.getZ()).getY();
-        if (overworldSpawnY != y) {
-            LogManager.getLogger("SetSpawn").warn("Coordinates given were impossible. Make sure the Y coordinate is possible. Not overriding player spawnpoint.");
-            return false;
-        }
-        return true;
-    }
-
-    @Nullable
-    private static BlockPos findOverworldSpawn(ServerWorld world, int x, int z) { // method taken from mc code
-        BlockPos.Mutable mutable = new BlockPos.Mutable(x, 0, z);
-        Biome biome = world.getBiome(mutable);
-        boolean bl = world.getDimension().hasCeiling();
-        BlockState blockState = biome.getGenerationSettings().getSurfaceConfig().getTopMaterial();
-        if (!blockState.getBlock().isIn(BlockTags.VALID_SPAWN)) {
-            return null;
-        } else {
-            WorldChunk worldChunk = world.getChunk(x >> 4, z >> 4);
-            int i = bl ? world.getChunkManager().getChunkGenerator().getSpawnHeight() : worldChunk.sampleHeightmap(Heightmap.Type.MOTION_BLOCKING, x & 15, z & 15);
-            if (i < 0) {
-                return null;
-            } else {
-                int j = worldChunk.sampleHeightmap(Heightmap.Type.WORLD_SURFACE, x & 15, z & 15);
-                if (j <= i && j > worldChunk.sampleHeightmap(Heightmap.Type.OCEAN_FLOOR, x & 15, z & 15)) {
-                    return null;
-                } else {
-                    for(int k = i + 1; k >= 0; --k) {
-                        mutable.set(x, k, z);
-                        BlockState blockState2 = world.getBlockState(mutable);
-                        if (!blockState2.getFluidState().isEmpty()) {
-                            break;
-                        }
-
-                        if (blockState2.equals(blockState)) {
-                            return mutable.up().toImmutable();
-                        }
-                    }
-
-                    return null;
-                }
-            }
-        }
+    public void setspawnmod_startedWorldGen(WorldGenerationProgressListener worldGenerationProgressListener, CallbackInfo ci){
+        SetSpawn.shouldModifySpawn=true;
     }
 }
 
